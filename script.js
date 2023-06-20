2902e5b8eb9a0c2021911c5827eccf34a2dc239a
EvertonAraujo88 <evertonaraujosenai@gmail.com>
2023-06-20 19:37:42 -0300

    atualizacao

---
+++ script.js
@@ -0,0 +1,15 @@
+let menu = document.getElementById("menu")
+let aside = document.getElementById("aside")
+let posicao = document.getElementById("posicao")
+
+function mostrarMenu() {
+    if (window.getComputedStyle(aside).left == "0px") {
+        aside.style.left = "-19vw"
+    }
+    else {
+        aside.style.left = "0px"
+        // aside.style.width = "50px"
+        // posicao.style.width = "20px"
+       
+    }
+}
