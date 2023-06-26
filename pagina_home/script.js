4d87b5638fc7b731217ea4088ed1f25e3c7b8652
Kauan <kauancph11@gmail.com>
2023-06-26 20:19:14 -0300

    26/06/23

--- pagina_home/script.js
+++ pagina_home/script.js
@@ -1,13 +1,15 @@
 let menu = document.getElementById("menu")
 let aside = document.getElementById("aside")
-let posicao = document.getElementById("posicao")
+let secao = document.getElementById("secao")
 
 function mostrarMenu() {
     if (window.getComputedStyle(aside).left == "0px") {
         aside.style.left = "-19vw"
+        secao.style.width = "100vw"
     }
     else {
         aside.style.left = "0px"
+        secao.style.width = "80vw"
         // aside.style.width = "50px"
         // posicao.style.width = "20px"
        
