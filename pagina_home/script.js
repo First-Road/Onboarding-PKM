78115aab1af9ced4e224c11447bccef5e40496ae
Kauan <kauancph11@gmail.com>
2023-06-27 21:48:33 -0300

    27/06/2023

--- pagina_home/script.js
+++ pagina_home/script.js
@@ -2,17 +2,30 @@ let menu = document.getElementById("menu")
 let aside = document.getElementById("aside")
 let secao = document.getElementById("secao")
 
+addEventListener("resize", () => {
+    if (window.innerWidth >= "769"){
+        aside.style.left = "0px"
+
+    }
+});
+
 function mostrarMenu() {
+   
     if (window.getComputedStyle(aside).left == "0px") {
-        aside.style.left = "-19vw"
-        secao.style.width = "100vw"
+
+            aside.style.left = "-19vw"       
     }
     else {
         aside.style.left = "0px"
-        secao.style.width = "80vw"
+           
+        // secao.style.width = "80vw"  
+        secao.style.width = "100vw"
+       
         // aside.style.width = "50px"
         // posicao.style.width = "20px"
        
     }
+
+    
 }
 
