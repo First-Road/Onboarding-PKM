dee8c4a27b562ab2dbf19ea21b0e7be4d80a8685
istic <istic@info.local>
2023-06-28 16:06:01 -0300

    28/06/23

---
+++ script.js
@@ -0,0 +1,20 @@
+let menu = document.getElementById("menu")
+let aside = document.getElementById("aside")
+
+
+addEventListener("resize", () => {
+    if (window.innerWidth >= "769"){
+        aside.style.left = "0px"
+    }
+});
+
+
+function mostrarMenu() {
+    if (window.getComputedStyle(aside).left == "0px") {
+        aside.style.left = "-19vw"
+    }
+    else {
+        aside.style.left = "0px"
+    }
+}
+
