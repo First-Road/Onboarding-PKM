80ec04c8e1a9395185115cf47cb89781c7e71b36
Kauan <kauancph11@gmail.com>
2023-06-28 21:28:01 -0300

    28/06/23

--- tela_conteudo/script.js
+++ tela_conteudo/script.js
@@ -2,6 +2,13 @@ let menu = document.getElementById("menu")
 let aside = document.getElementById("aside")
 let posicao = document.getElementById("posicao")
 
+addEventListener("resize", () => {
+    if (window.innerWidth >= "769"){
+        aside.style.left = "0px"
+
+    }
+});
+
 function mostrarMenu() {
     if (window.getComputedStyle(aside).left == "0px") {
         aside.style.left = "-19vw"
