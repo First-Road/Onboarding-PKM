30cf426b733adb72f89326787552d0a09d69f7af
Roberto Nascimento <tanjoroberuto@gmail.com>
2023-06-30 19:39:36 -0300

    reinicia pagina

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
 let menu = document.getElementById("menu")
 let aside = document.getElementById("aside")
-let sombra = document.getElementById("sombra")
-let body = document.getElementById("body")
-
 
 
 addEventListener("resize", () => {
-    if (window.innerWidth >= "768"){
+    if (window.innerWidth >= "769"){
         aside.style.left = "0px"
-    }
-    else{
-        aside.style.left = "-140px"
-
     }
 });
 
 
 function mostrarMenu() {
     if (window.getComputedStyle(aside).left == "0px") {
-        aside.style.left = "-140px"
-        sombra.style.right ="110vw"
-        body.style.overflow = "auto"
+        aside.style.left = "-19vw"
     }
     else {
         aside.style.left = "0px"
-        sombra.style.right = "0px"
-        body.style.overflow = "hidden"
     }
 }
 
