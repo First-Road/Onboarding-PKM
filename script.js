d6fcc419deddc88221a56ca69dea23fc66e1c2c3
robbertonascimento51 <tanjoroberuto@gmail.com>
2023-06-30 17:01:48 -0300

    pagina adminLogado terminada na versões:Desktop, Tablet e mobile BUG encontrado no aside

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let posicao = document.getElementById("posicao")
 
 function mostrarMenu() {
     if (window.getComputedStyle(aside).left == "0px") {
-        aside.style.left = "-19vw"
+        aside.style.left = "-49vw"
     }
     else {
         aside.style.left = "0px"
