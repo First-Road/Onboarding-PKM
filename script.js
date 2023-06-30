4a1a462467c00ce6860e3c3ff1b6145077c7a8e5
LucasLessa <lessa07oliveira@gmail.com>
2023-06-29 21:26:49 -0300

    segundo commit

--- script.js
+++ script.js
@@ -8,50 +8,10 @@ function mostrarMenu() {
     }
     else {
         aside.style.left = "0px"
-        // aside.style.width = "50px"
-        // posicao.style.width = "20px"
-       
+    
     }
 }
 
-// const botaoAnimar = document.getElementById("animar");
-// const botaoAnimar1 = document.getElementById("animar1");
-// const botaoAnimar2 = document.getElementById("animar2");
-// const botaoAnimar3 = document.getElementById("animar3");
-// const botaoAnimar4 = document.getElementById("animar4");
-
-// const carro = document.querySelector(".carro");
-
-// botaoAnimar.addEventListener("click", ()=> {
-//   carro.style.animation = "";
-//   setTimeout(() => carro.style.animation = "veic1 5s ease-in-out");
-//   carro.style.top = "0px";
-//   carro.style.right = "90px";
-// });
-
-// botaoAnimar1.addEventListener("click", ()=> {
-//   carro.style.animation = "";
-//   setTimeout(() => carro.style.animation = "veic2 5s ease-in-out");
-//   carro.style.top = "0px";
-//   carro.style.right = "300px";
-// });
-
-// botaoAnimar2.addEventListener("click", ()=> {
-//   carro.style.animation = "";
-//   setTimeout(() => carro.style.animation = "veic3 5s ease-in-out");
-//   carro.style.top = "0px";
-//   carro.style.right = "300px";
-// });
-
-// botaoAnimar3.addEventListener("click", ()=> {
-//   carro.style.animation = "";
-//   setTimeout(() => carro.style.animation = "veic4 5s ease-in-out");
-// });
-
-// botaoAnimar4.addEventListener("click", ()=> {
-//   carro.style.animation = "";
-//   setTimeout(() => carro.style.animation = "veic5 5s ease-in-out");
-// });
 
 
 
