7b604634db8ea4bf04763b57117917c54d817d2f
Roberto Nascimento <tanjoroberuto@gmail.com>
2023-06-30 21:09:17 -0300

    Pagina adminDesloagado desktop finalizada

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let menu = document.getElementById("menu")
 let aside = document.getElementById("aside")
-let posicao = document.getElementById("posicao")
+let sombra = document.getElementById("sombra")
+let body = document.getElementById("body")
+
+
+
+addEventListener("resize", () => {
+    if (window.innerWidth >= "768"){
+        aside.style.left = "0px"
+    }
+    else{
+        aside.style.left = "-140px"
+
+    }
+});
+
 
 function mostrarMenu() {
     if (window.getComputedStyle(aside).left == "0px") {
-        aside.style.left = "-19vw"
+        aside.style.left = "-140px"
+        sombra.style.right ="110vw"
+        body.style.overflow = "auto"
     }
     else {
         aside.style.left = "0px"
-        // aside.style.width = "50px"
-        // posicao.style.width = "20px"
-       
+        sombra.style.right = "0px"
+        body.style.overflow = "hidden"
     }
 }
-
